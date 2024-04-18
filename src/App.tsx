57f1a320ab3c72abac5a036ad5617bf55fa17d7b
kelvin511 <mailto:102317221+kelvin511@users.noreply.github.com>
2024-04-18 15:36:42 +0530

    final

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 import { Swiper, SwiperSlide } from "swiper/react"
 import { EffectCoverflow } from "swiper/modules"
 import { Autoplay } from "swiper/modules"
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules"
+
 import "swiper/css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
